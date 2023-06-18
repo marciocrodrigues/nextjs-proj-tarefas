74f5cf6078514b913ab0196b75dcaf00181d414b
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-06-18 19:42:20 -0300

    testando tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <div>TESTE</div>;
+  return (
+    <div
+      className={`
+      flex flex-col justify-center items-center
+      text-white
+      bg-gradient-to-tr from-purple-500 to-yellow-600 bg-purple-600
+      h-screen
+    `}
+    >
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+    </div>
+  );
 }
