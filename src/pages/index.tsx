2d58f32e6ca148ffbf42f7a2602df2b6f8adc76f
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-06-18 22:15:47 -0300

    finalizada modelagem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,33 @@
+import tarefasIniciais from "../data/mock";
+import Tarefa from "../model/Tarefa";
+
 export default function Home() {
+  let tarefas = tarefasIniciais;
+  tarefas = tarefas.filtrarAtivas();
+  tarefas = tarefas.filtrarConcluidas();
+  tarefas = tarefas.removerFiltro();
+  //tarefas = tarefas.excluirConcluidas();
+  tarefas = tarefas.adicionarTarefa(
+    Tarefa.criarConcluida(4, "Lavar os pratos")
+  );
+  tarefas = tarefas.adicionarTarefa(
+    Tarefa.criarAtiva(5, "Cuidar das crianças")
+  );
+
+  tarefas = tarefas.modificarTarefa(tarefas.itens[4].alternarStatus());
+
+  function renderizarTarefas() {
+    return tarefas.itens.map((tarefa) => {
+      return (
+        <div key={tarefa.id}>
+          <span>{tarefa.id} | </span>
+          <span>{tarefa.descricao} | </span>
+          <span>{tarefa.ativa ? "Ativa" : "Concluída"}</span>
+        </div>
+      );
+    });
+  }
+
   return (
     <div
       className={`
@@ -8,9 +37,7 @@ export default function Home() {
       h-screen
     `}
     >
-      <span>1</span>
-      <span>2</span>
-      <span>3</span>
+      {renderizarTarefas()}
     </div>
   );
 }
