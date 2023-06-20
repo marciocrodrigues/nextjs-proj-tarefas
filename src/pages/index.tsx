9c0f7d5afb4d80492ffea7c4e82a146b39c881ea
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-06-19 23:01:37 -0300

    Componente ListaItem e Selecao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,27 @@
-import tarefasIniciais from "../data/mock";
-import Tarefa from "../model/Tarefa";
+import ListaItem from "../components/lista/ListaItem";
 
 export default function Home() {
-  let tarefas = tarefasIniciais;
-  tarefas = tarefas.filtrarAtivas();
-  tarefas = tarefas.filtrarConcluidas();
-  tarefas = tarefas.removerFiltro();
-  //tarefas = tarefas.excluirConcluidas();
-  tarefas = tarefas.adicionarTarefa(
-    Tarefa.criarConcluida(4, "Lavar os pratos")
-  );
-  tarefas = tarefas.adicionarTarefa(
-    Tarefa.criarAtiva(5, "Cuidar das crianças")
-  );
-
-  tarefas = tarefas.modificarTarefa(tarefas.itens[4].alternarStatus());
-
-  function renderizarTarefas() {
-    return tarefas.itens.map((tarefa) => {
-      return (
-        <div key={tarefa.id}>
-          <span>{tarefa.id} | </span>
-          <span>{tarefa.descricao} | </span>
-          <span>{tarefa.ativa ? "Ativa" : "Concluída"}</span>
-        </div>
-      );
-    });
-  }
-
   return (
     <div
       className={`
       flex flex-col justify-center items-center
       text-white
-      bg-gradient-to-tr from-purple-500 to-yellow-600 bg-purple-600
+      bg-gradient-to-tr from-gray-600 to-gray-900
       h-screen
     `}
     >
-      {renderizarTarefas()}
+      <ul>
+        <ListaItem
+          valor="Exemplo de Item #01"
+          concluido={false}
+          alterarStatus={() => {}}
+        />
+        <ListaItem
+          valor="Exemplo de Item #02"
+          concluido={true}
+          alterarStatus={() => {}}
+        />
+      </ul>
     </div>
   );
 }
